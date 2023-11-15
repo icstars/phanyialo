30f705e2b8baca5accb2a8c62c286bb3a7b6c13e
phanyialo <plo@icstars.org>
2023-11-15 15:28:33 -0600

    changed h1
    changed font family and added shadow

---
+++ code.js
@@ -0,0 +1,168 @@
+//let arr = [1,2,3,4,5]
+
+//let lastItem = arr.slice(-1)
+
+//let num = lastItem.slice(0)
+
+//num = 9
+
+//arr[arr.length - 1]
+
+// console.log(arr)
+
+//const arr1 = [5,6,2,9]
+
+//arr1[1]
+
+//console.log(arr1)
+//arr1.push(arr1)
+
+//arr.pop()
+//console.log(arr1)
+
+//Math.random()
+
+//console.log(Math.random() * arr.length)
+
+//console.log(arr1[Math.floor(Math.random() * arr1.length)])
+
+//let arr = [2,4,5,6,2,4]
+//let len = Math.random() * arr.length
+//console.log(arr[Math.floor(len)])
+
+
+//let arr = [3,6,9,12,15,18,21]
+
+//let randomNumber = Math.floor(Math.random() * arr.length)
+//console.log(randomNumber)
+
+
+
+// FOR LOOPS
+// It repeats a task or code a certain number of times 
+// based on a condition.
+
+// split() - splits a string by the specified character
+// useful to split some data from a string
+
+// join() - joins array elements between a specified character
+// useful to join data together
+
+//let arr = [1, 2, 3]
+
+//console.log(arr.join(''))
+
+
+//let str = 'coallain'
+//console.log(str.split('a'))
+
+
+// Traditional //
+// 1. initializer 2. condition 3. iterator
+
+
+//for (let i = 0; i < arr.length; i++) {
+//    console.log(i)
+//}
+
+
+//for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i])
+//}
+
+
+
+// for of loop
+
+//for (char of arr) {
+//    console.log(char)
+//}
+
+//let string = 'Phanyia'
+//for (char of string) {
+//    console.log(char)
+//}
+
+
+// while loop
+
+//let j = 0
+//while (j < 5) {
+//    j++
+//    console.log(j)
+//}
+
+
+//let word = 'bye'
+//let attempts = 0
+//let arr = []
+//let characters = 'abcdefghijklmnopqrstuvwxyz'
+//let charArray = characters.split('')
+
+
+//while (arr.join(``) != word) {
+//    arr = []
+//    for (char of word) {
+//        arr.push()
+//    }
+//}
+
+//console.log(charArray)
+
+//let arr = []
+//let characters = ('collin', 2, 3, 4, 5)
+
+//while (arr.join(``) != word) {
+//    arr=['collin', 2, 3, 4, 5]
+//    for (char of word) {
+//    }
+//}
+
+//console.log(characters)
+
+
+//while (arr.join(``) != word) {
+//    arr = []
+//    for (char of word) {
+//    arr.push(charArray[Math.floor(Math.random() * charArray.length)])
+//    }
+//    console.log(arr.join(``))
+//    attempts++
+//}
+
+ //console.log(`Number of attempts: ${attempts}`)
+
+
+//let numbers = [4,1,10,100,32,5]
+
+//for (let i = 0; i < numbers.length; i++) {
+//    console.log(i)
+//}
+
+//const arr = ['collin',2,3,4,5]
+// for (let i = 0; i <arr.length; i++) {
+//    console.log(arr[i])
+// }
+
+
+//let p = 0
+//while (p < 5) {
+//    p++
+//    console.log(p)
+//}
+
+
+//const arr = ['collin', 2, 3, 4, 5];
+
+//let i = 0;
+//let text = "";
+//while (arr[i]) {
+//    text += arr[i] + "<br>";
+//    i++;
+//}
+
+//console.log(arr[i])
+
+let array = [1,2,3]
+
+while()
--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 
 
 //  let - are mutable / values that can change
-let number = 15
+//let number = 15
 
-number = 10
+//number = 10
 
-console.log(number)
+//console.log(number)
 
 // console.log is putting it into the consoles
 
 // const / immutable / cannot change
-const name = 'Collin'
+//const name = 'Collin'
 
 // 'Collin' - a string, boolean, numbers
 // because it's on the right side of the '='
@@ -21,62 +21,62 @@ const name = 'Collin'
 
 // var - won't really use
 
-var number2 = 0 
+//var number2 = 0 
 
-number2 = 10
+//number2 = 10
 
 //  Data Types
 
 // Number/integer
-let x = 2
+//let x = 2
 
-let y = 2.5
+//let y = 2.5
 
-let z = x + y 
+//let z = x + y 
 
-let b = 1000000
+//let b = 1000000
 
-console.log(2**4)
+//console.log(2**4)
 
 // 2**4 means 2 to the power of 4. this is used
 // for gaming 
 
 // String - sequence of characters
 // '', "", ``
-const name2 = "You're"
+//const name2 = "You're"
 
-console.log(name2)
+//console.log(name2)
 
 // Concatenation - adding two strings together
-let sentence = 'hi'
-let sentence2 = 'bye'
-let sentence3 = sentence+sentence2
-let sentence4 = sentence + ' ' + sentence2
-let sentence5 = sentence + sentence2
-let sentence6 = `Hi, my name is PhaNyia`
+//let sentence = 'hi'
+//let sentence2 = 'bye'
+//let sentence3 = sentence+sentence2
+//let sentence4 = sentence + ' ' + sentence2
+//let sentence5 = sentence + sentence2
+//let sentence6 = `Hi, my name is PhaNyia`
 // ${} - template literal - always use bat tics
-let sentence7 = `Hi, my name is PhaNyia. ${sentence2}`
+//let sentence7 = `Hi, my name is PhaNyia. ${sentence2}`
 
-console.log(sentence3)
-console.log(sentence4)
-console.log(sentence5)
-console.log(sentence6)
+//console.log(sentence3)
+//console.log(sentence4)
+//console.log(sentence5)
+//console.log(sentence6)
 // ${} - template literal - always use bat tics
-console.log(sentence7)
+//console.log(sentence7)
 
-let o =200
+//let o =200
 
-let s = `${o}`
+//let s = `${o}`
 
-let num = '20,000'
+//let num0 = '20,000'
 
-let num2 = 20
+//let num2 = 20
 
-console.log(num + num2)
+//console.log(num0 + num2)
 
-const num3 = 10
+//const num3 = 10
 
-console.log(num3) // prints to the console
+//console.log(num3) // prints to the console
 
 // Booleans
 
@@ -95,63 +95,85 @@ console.log(num3) // prints to the console
 // console.log(Boolean()) - can let you know if
 // something is true or false
 // negative numbers will still be a truthy
-console.log(Boolean(-1))
+//console.log(Boolean(-1))
 
 
-let sentence8 = `Hi, my name is ${name} and I am ${num3}` 
+//let sentence8 = `Hi, my name is ${name} and I am ${num3}` 
 
 
 
 
 // what is an array ? - a list of data
-let emptyArr = []
-console.log(emptyArr);
-const lengthOfEmptyArray = emptyArr.length;
-console.log(lengthOfEmptyArray);
+//let emptyArr = []
+//console.log(emptyArr);
+//const lengthOfEmptyArray = emptyArr.length;
+//console.log(lengthOfEmptyArray);
 
-const mixArr = [1, "Collin"]
-console.log(mixArr)
+//const mixArr = [1, "Collin"]
+//console.log(mixArr)
 
-const name4 = "Lucycan";
+//const name4 = "Lucycan";
 
-const nameArr = ["Lucycan", "Collin", "Mark"];
-console.log(nameArr);
-const lengthOfNameArr = nameArr.length;
-console.log(lengthOfNameArr); 
+//const nameArr = ["Lucycan", "Collin", "Mark"];
+//console.log(nameArr);
+//const lengthOfNameArr = nameArr.length;
+//console.log(lengthOfNameArr); 
 
-const hello = `Hi, my name is ${nameArr[1]}.`;
-console.log(nameArr[1])
+//const hello = `Hi, my name is ${nameArr[1]}.`;
+//console.log(nameArr[1])
 
 
 // submitting form
-nameArr.push("Cortez");
-nameArr.push("Romeo")
-console.log(nameArr);
+//nameArr.push("Cortez");
+//nameArr.push("Romeo")
+//console.log(nameArr);
 
 // grabs the most recent submission so it will grab "Romeo"
-console.log(nameArr.pop());
+//console.log(nameArr.pop());
 
 
 // push() - this will place each one on the array
 
-const values = ['a', 'b', 'c'];
-values.push('d', 'e', 'f');
-console.log(values)
+//const values = ['a', 'b', 'c'];
+//values.push('d', 'e', 'f');
+//console.log(values)
 
 // pop() - this will pop off the last value
 
-const last = values.pop();
-console.log(values, last);
+//const last = values.pop();
+//console.log(values, last);
 
 
 // shift() - this will kick first element out of array.
 // new array will be set to 
 // ['b', 'c', 'd', 'e', 'f'] "a"
-const first = values.shift();
-console.log( values, first);
+//const first = values.shift();
+//console.log( values, first);
 
 // unshift() - this will log value to the 
 // beginning of the array
 // ['hello', 'a', 'b', 'c', 'd', 'e']
-values.unshift('hello');
-console.log(values);
+//values.unshift('hello');
+//console.log(values);
+
+
+//let arr = [1,2,3,4,5]
+
+//let lastItem = arr.slice(-1)
+
+//let num = lastItem.slice(0)
+
+//console.log(arr, num)
+
+//let input = prompt("what is your name?")
+//let input2 = prompt("What is your age?")
+//alert("Hi, my name is" + " " + input + " " + "and I am" + " " + input2 + " " + "years old.")
+//alert(`Hi, my name is ${input} and I am ${input2} years old.`)
+
+
+//let guess = prompt ('Guess a number')
+
+//while(guess != 3) {
+//    guess = prompt("Try again.")
+//}
+//alert("You guessed correctly")
